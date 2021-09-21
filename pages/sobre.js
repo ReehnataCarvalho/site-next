da67131abde7287ac30afdd00fd1ad99a67eb405
Renata Carvalho <renata.carvalho@mutant.com.br>
2021-09-21 00:29:31 -0300

    Criando conexao entre as paginas

---
+++ pages/sobre.js
@@ -0,0 +1,15 @@
+import Link from 'next/link'
+
+function Home() {
+  return (
+    <div>
+      <h1>Sobre</h1>
+
+      <Link href="/">
+        <a>Acessar página Home</a>
+      </Link>
+    </div>
+  )
+}
+
+export default Home
